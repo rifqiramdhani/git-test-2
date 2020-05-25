8e0b82d18e97620212bfb20e19bb65c0ae71c5c3
Rifqi Ramdhani <rifqiramdhani71@yahoo.co.id>
2020-05-26 04:27:35 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h2>Hello world</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam sequi, rerum cumque ex amet harum doloremque illo inventore itaque natus dolorem deleniti consequuntur provident laudantium animi voluptas? Ex, commodi.</p>
+    <ul>
+        <li>Wayae
+        </li>
+    </ul>
 </body>
 </html>
